a0acac06ca2fe75522a4a4d42bdad43b345b41e2
Will Keleher <will.keleher@gmail.com>
2014-01-08 21:31:05 -0800

    login and index.html

--- server.js
+++ server.js
@@ -4,44 +4,57 @@ var express = require('express');
 
 var app = express();
 
+//Configuring app--passport needs cookieParser, session, and passport stuff
 app.configure(function(){
   app.use(express.bodyParser());
   app.use('/images', express.static(__dirname + '/images'));
   app.use('/styles', express.static(__dirname + '/styles'));
+  app.use('/bower_components', express.static(__dirname + '/bower_components'));
   app.use(express.static(__dirname + '/public'));
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  //TODO change to an actual secret later
+  app.use(express.session({ secret: 'something secret' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
 });
 
 passport.use(new LocalStrategy(
   function(username, password, done) {
+    console.log('using local strategy', username, password);
     username = username || 'username';
-    //TAKE THIS OUT FOR PASSWORD CHECKING
-    if (password === process.env.PASSWORD || true){
-     return done(null, true);
+    //TODO get rid of true
+    if (true || password === process.env.PASSWORD){
+     return done(null, username);
     } else {
      return done(null, false);
- }
+    }
   }
 ));
 
-app.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' }));
-
-app.get('/login', function(request, response){
-  response.sendfile('login.html');
+//  This code is necessary for passport to keep state (it's not really important for this application)
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(id, done) {
+  console.log('deserializing', id);
+  done(null, id);
 });
 
+app.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' }));
+
+app.get('/login', function(request, response){  response.sendfile('login.html'); });
+
 app.get('*', function(request, response){
-  if (request.user){
-    handle(request, response);
-  } else {
-    response.redirect('/login');
-  }
+  if (request.user) handle(request, response);
+  else response.redirect('/login');
 });
 
 var handle = function(request, response){
-  console.log('looks like you logged in! Congratulations');
-  var url = request.url;
-  console.log(url);
+  var url = request.url.toLowerCase();
+  console.log('logged in with url ', url);
+  if (url === '/') response.sendfile('./index.html');
 };
-
 
 app.listen(process.env.PORT || 3000);
